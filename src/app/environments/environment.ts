0eaaebf7e598f616ff8976826bec156245be5564
Amichai Friedman <afriedman@paloaltonetworks.com>
2025-09-17 22:37:10 +0300

    trying again

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  baseUrl: '/api',
+  baseUrl: 'https://tri-sphere-budget-be.onrender.com',
 };
 
 /*
