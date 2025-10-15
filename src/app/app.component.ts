94f8efd58b1e183514ad419d3da893ee1974e12f
Amichai Friedman <afriedman@paloaltonetworks.com>
2025-10-15 15:15:11 +0300

    Renamed to user facing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'tri-sphere-budget';
+  title = 'Budget Compass';
   isSidenavOpen = false;
 
   toggleSidenav() {
